b73c0ee4dd35f8b6428c4b2f02f5eada6db677d2
Sam Goddard <hello@samgoddard.co.uk>
2021-05-16 18:40:54 +0100

    first pass at Sanity integration

--- pages/_app.js
+++ pages/_app.js
@@ -7,10 +7,13 @@ export default function App({ Component, pageProps }) {
   const router = useRouter()
 
   return (
+    <>
+    { pageProps.preview && <><div className={'fixed bottom-0 w-full p-1 bg-red bg-opacity-100 text-white justify-center flex z-30'}>Preview Mode - <a className={'px-1 underline'} href={`/api/exit-preview?currentRoute=${router.route}`}>Click Here To Exit</a></div></> }
     <div data-scroll-container id="scroll-container">
       <AnimatePresence exitBeforeEnter initial={false}>
         <Component {...pageProps} key={router.asPath} />
       </AnimatePresence>
     </div>
+    </>
   )
 }
